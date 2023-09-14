9c8b87b383ba5e6e74582181591a10f2a1d1ba07
aysezynp <128591659+aysezynp@users.noreply.github.com>
2023-09-14 10:01:15 +0300

    last commit

--- src/test/java/api/exlab/common/DataForApi.java
+++ src/test/java/api/exlab/common/DataForApi.java
@@ -1,2 +1,48 @@
-package api.exlab.common;public class DataForAPI {
-}
+package api.exlab.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataForApi {
+    /**{
+     "name": "{{$randomFirstName}}",
+     "email": "{{$randomEmail}}",
+     "password": "Test1234",
+     "about": "About Me",
+     "terms": "10"
+     }*/
+
+    public static Map<String,Object> registerUserBody(String name,String email,String password,String about,String terms) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("name", name);
+        requestMap.put("email", email);
+        requestMap.put("password", password);
+        requestMap.put("about", about);
+        requestMap.put("terms", terms);
+        return requestMap;
+    }
+    /**"job": "Junior Developer",
+            "company": "{{$randomCompanyName}}",
+            "location": "{{$randomCountry}}",
+            "fromdate": "2023-01-01",
+            "todate": "2023-08-08",
+            "current": "false",
+            "description": "Description"
+    }*/
+
+       public static Map <String,Object> userExperienceBody(String job,
+               String company,String location,String fromdate,String todate,String current, String description){
+           Map<String, Object> experienceBody=new HashMap<>();
+           experienceBody.put("job",job);
+           experienceBody.put("company",company);
+           experienceBody.put("location",location);
+           experienceBody.put("fromdate",fromdate);
+           experienceBody.put("todate",todate);
+           experienceBody.put("current",current);
+           experienceBody.put("description",description);
+           return  experienceBody;
+       }
+
+
+   }
+
--- src/test/java/api/exlab/requests/ExlabRequest.java
+++ src/test/java/api/exlab/requests/ExlabRequest.java
@@ -1,42 +1,53 @@
 package api.exlab.requests;
-import api.exlab.common.DataForAPI;
+
+import api.exlab.common.DataForApi;
+import api.exlab.utilities.ApiUtils;
 import api.exlab.utilities.ConfigurationReader;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 
-public class ExlabRequests {
-
-    public static Response response;
+public class ExlabRequest {
+    public static  Response response;
     public static int userID;
-    public static String token;
+    public  static String token;
+    public  static String email;
+    public static int experience_ID;
+    public static String experience_job;
+
 
     public static Response registerNewUser(String name,String email,String password,String about,String terms){
-      response= given().contentType(ContentType.JSON)
+        response = given().accept(ContentType.JSON)
+                .and()
                 .contentType(ContentType.JSON)
-                .body(DataForAPI.registerUSerBody(name, email, password, about, terms))
+                .and()
+                .body(DataForApi.registerUserBody(name, email, password, about, terms))
                 .when()
                 .post(ConfigurationReader.get("postRegisterUser"));
+        userID=response.path("id");
+        System.out.println("userID = " + userID);
+        token=response.path("token");
+        email=response.path("email");
 
-      userID=response.path("id");
-      token=response.path("token");
-
-      return response;
+        return  response;
     }
 
     public static Response postExperience(String job,
                                           String company,String location,String fromdate,String todate,String current, String description){
-
         response=given().accept(ContentType.JSON)
-                .header()
                 .and()
-                .body(DataForAPI.userExperienceBody(job, company, location, fromdate, todate, current, description))
+                .headers(ApiUtils.getToken(ConfigurationReader.get("email"),ConfigurationReader.get("password")))
+                //.headers(ApiUtils.getToken(email,ConfigurationReader.get("password")))
+                .and()
+                .body(DataForApi.userExperienceBody(job, company, location, fromdate, todate, current, description))
                 .when()
+                .post(ConfigurationReader.get("postExperience"));
 
+        experience_ID=response.path("id");
+        experience_job=response.path("job");
 
-
+        return  response;
     }
-
 
 }
